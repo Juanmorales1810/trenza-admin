99fd77f46e6545ed7b570d4d06b3cb356dc526a4
Juan Domingo Exequiel Morales <85465719+Juanmorales1810@users.noreply.github.com>
2024-11-10 23:53:53 -0300

    Feat: Mejorar la barra lateral de administración con enlaces dinámicos y navegación

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -1,5 +1,6 @@
-import { BookMarked, BriefcaseBusiness, Building2, ChevronDown, Gem, NotebookPen } from "lucide-react"
+"use client";
 
+import { BookMarked, BriefcaseBusiness, Building2, ChevronDown, Gem, Home, NotebookPen } from "lucide-react"
 import {
     Sidebar,
     SidebarContent,
@@ -11,9 +12,12 @@ import {
     SidebarMenuItem,
 } from "@/components/ui/sidebar"
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "./ui/dropdown-menu"
+import Link from "next/link"
+import { usePathname } from 'next/navigation';
 
 // Menu items.
 const items = [
+
     {
         title: "Blog",
         url: "#",
@@ -27,11 +31,14 @@ const items = [
 ]
 
 export function AppSidebar() {
+    const router = usePathname();
+
+
     return (
         <Sidebar>
             <SidebarContent>
                 <SidebarGroup>
-                    {/* <SidebarGroupLabel>Menu Trenza</SidebarGroupLabel> */}
+                    {/* <SidebarGroupLabel>{router}</SidebarGroupLabel> */}
                     <SidebarGroupContent>
                         <SidebarMenu>
                             <SidebarMenuItem>
@@ -40,36 +47,68 @@ export function AppSidebar() {
                                         <SidebarMenuButton className="bg-zinc-700 h-12">
                                             <div className="flex justify-center items-center gap-2">
                                                 <div className="p-1 bg-zinc-500 rounded-md">
-                                                    <Building2 />
+                                                    {
+                                                        router === "/admin/trenza-matrimonios" ? <Gem /> :
+                                                            router === "/admin/trenza-estudio" ? <BookMarked /> : <Building2 />
+                                                    }
                                                 </div>
-                                                <span>Selecciona la empresa</span>
+                                                <span>
+                                                    {
+                                                        router === "/admin/trenza-matrimonios" ? "Trenza Matrimonios" :
+                                                            router === "/admin/trenza-estudio" ? "Trenza Estudio" : "Selecciona la empresa"
+                                                    }
+                                                </span>
                                             </div>
                                             <ChevronDown className="ml-auto" />
                                         </SidebarMenuButton>
                                     </DropdownMenuTrigger>
                                     <DropdownMenuContent side="left" className="w-[--radix-popper-anchor-width] mt-2">
-                                        <DropdownMenuItem>
-                                            <Gem />
-                                            <span>Trenza Matrimonios</span>
+                                        <DropdownMenuItem >
+                                            <Link href="/admin/trenza-matrimonios" className="flex justify-center items-center gap-2">
+                                                <Gem />
+                                                <span>Trenza Matrimonios</span>
+                                            </Link>
                                         </DropdownMenuItem>
                                         <DropdownMenuItem>
-                                            <BookMarked />
-                                            <span>Trenza Estudio</span>
+                                            <Link href="/admin/trenza-matrimonios" className="flex justify-center items-center gap-2">
+                                                <BookMarked />
+                                                <span>Trenza Estudio</span>
+                                            </Link>
                                         </DropdownMenuItem>
                                     </DropdownMenuContent>
                                 </DropdownMenu>
                             </SidebarMenuItem>
+                            <SidebarMenuItem >
+                                <SidebarMenuButton asChild>
+                                    <Link href="/admin">
+                                        <Home />
+                                        <span>Inicio</span>
+                                    </Link>
+                                </SidebarMenuButton>
+                            </SidebarMenuItem>
 
-                            {items.map((item) => (
-                                <SidebarMenuItem key={item.title}>
-                                    <SidebarMenuButton asChild>
-                                        <a href={item.url}>
-                                            <item.icon />
-                                            <span>{item.title}</span>
-                                        </a>
-                                    </SidebarMenuButton>
-                                </SidebarMenuItem>
-                            ))}
+                            {
+                                router === "/admin/trenza-matrimonios" ? items.map((item) => (
+                                    <SidebarMenuItem key={item.title}>
+                                        <SidebarMenuButton asChild>
+                                            <Link href={item.url}>
+                                                <item.icon />
+                                                <span>{item.title}</span>
+                                            </Link>
+                                        </SidebarMenuButton>
+                                    </SidebarMenuItem>
+                                )) : router === "/admin/trenza-estudio" ? items.map((item) => (
+                                    <SidebarMenuItem key={item.title}>
+                                        <SidebarMenuButton asChild>
+                                            <Link href={item.url}>
+                                                <item.icon />
+                                                <span>{item.title}</span>
+                                            </Link>
+                                        </SidebarMenuButton>
+                                    </SidebarMenuItem>
+                                )) :
+                                    null
+                            }
                         </SidebarMenu>
                     </SidebarGroupContent>
                 </SidebarGroup>
