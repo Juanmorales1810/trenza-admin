e32404ed051a83ea8d530fb1c32502bedf4740be
Juan Domingo Exequiel Morales <85465719+Juanmorales1810@users.noreply.github.com>
2024-11-22 23:33:44 -0300

    Feat: Agregar enlaces a Blogs y Portfolio en la página de administración de matrimonios

--- app/admin/trenza-matrimonios/page.tsx
+++ app/admin/trenza-matrimonios/page.tsx
@@ -1,8 +1,23 @@
+import Link from "next/link";
 
 export default function page() {
     return (
         <div>
-            <h1>En proceso</h1>
+
+            <div className="flex space-x-6">
+                <Link href="/admin/trenza-matrimonios/blog">
+                    <article className="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100">
+                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">Blogs</h5>
+                        <p className="font-normal text-gray-700">Mira tus últimos blogs creados</p>
+                    </article>
+                </Link>
+                <Link href="/admin/trenza-matrimonios/portfolio">
+                    <article className="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100">
+                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">Portfolio</h5>
+                        <p className="font-normal text-gray-700">Explora tu portafolio</p>
+                    </article>
+                </Link>
+            </div>
         </div>
     )
 }
