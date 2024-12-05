493847ca00cce6430e8a932e4cb2e65e86dac719
Juan Domingo Exequiel Morales <85465719+Juanmorales1810@users.noreply.github.com>
2024-12-04 23:22:41 -0300

    Feat: Implementar carga dinámica de portafolios en la página de administración

--- app/admin/trenza-matrimonios/portfolio/page.tsx
+++ app/admin/trenza-matrimonios/portfolio/page.tsx
@@ -1,8 +1,44 @@
+import { TrenzaMatrimoniosPortfolioModel } from "@/models/trenza";
+import { connectMongoDB } from "@/lib/mongodb";
+import CardBlog from "@/components/cardBlog";
+import { cache } from "react";
+import CardPortfolio from "@/components/cardPortfolio";
+import { format } from "date-fns";
+import { es } from "date-fns/locale";
 
-export default function page() {
+export const dynamic = "force-dynamic";
+
+const getItems = cache(async function loadMenu() {
+    await connectMongoDB();
+    const ListPortfolios = await TrenzaMatrimoniosPortfolioModel.find();
+    return ListPortfolios.map(portfolio => {
+        const obj = portfolio.toObject();
+        obj._id = obj._id.toString(); // Convierte _id a una cadena
+        return obj;
+    }); // Usa .toObject() para convertir cada producto a un objeto JavaScript simple
+})
+export default async function page() {
+    const portfolios = await getItems();
+
     return (
-        <div>
-            <h1>En proceso</h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-full max-w-7xl gap-16 mx-auto ">
+            {
+                portfolios.length === 0 && (
+                    <div className="w-full text-center">
+                        <h1>No hay porfolios</h1>
+                    </div>
+                )
+            }
+            {portfolios.map((portfolio) => (
+                <CardPortfolio
+                    key={portfolio._id}
+                    title={portfolio.title}
+                    cardImage={portfolio.image}
+                    date={format(portfolio.date, "PPP", { locale: es })}
+                    location={portfolio.location}
+                />
+            ))
+            }
         </div>
     )
 }
